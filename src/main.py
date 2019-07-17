a9c6c519b2d4bc1638b5d8c453d38e0a6749ccce
Ritchy <blezin.ritchy@gmail.com>
2019-07-17 15:03:12 +0000

    Ajout: Menu.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,30 @@
-if __name__ == '__main__':
-    pass
+from getpass import getpass
 
+
+def ask_for_choice():
+    """
+    Ask for choice in the main menu.
+    """
+    while True:
+        try:
+            message_prompt = '-------------------------------------------\n'\
+                             '1 - Quel aliment souhaitez-vous remplacer ?\n'\
+                             '2 - Retrouver mes aliments substitués.\n'\
+                             'q - Quitter.\n'\
+                             '-------------------------------------------\n'
+            choice = getpass(prompt=message_prompt)
+            if choice == '1':
+                print('C\'est gagné ! 1')
+            elif choice == '2':
+                print('C\'est gagné ! 2')
+            elif choice == 'q':
+                print('\nFermeture du logiciel\n')
+                exit()
+            else:
+                raise ValueError
+        except ValueError:
+            print(f'\'{choice}\' n\'est pas valide.\n')
+
+
+if __name__ == '__main__':
+    ask_for_choice()
