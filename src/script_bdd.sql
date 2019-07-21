018953b0cdcce3b8e804eeb8b3706063152292a0
mnrdev <mnrdev@mnrs-macbook-pro-1.home>
2019-07-21 20:28:35 +0200

    Ajout: Script de creation de la base de données.

---
+++ src/script_bdd.sql
@@ -0,0 +1,6 @@
+/* User creation */
+/*CREATE USER 'newuser'@'localhost' IDENTIFIED BY '1234';
+GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'localhost' IDENTIFIED BY '1234';*/
+
+/* Database creation */
+CREATE DATABASE yuka_like CHARACTER SET 'utf8';
