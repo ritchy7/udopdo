0325a70402d698e279f8676822796330a77782c5
mnrdev <mnrdev@mnrs-macbook-pro-1.home>
2019-07-21 20:34:59 +0200

    Modification: Nom de l'utilisateur, nom de la base de données.

--- src/script_bdd.sql
+++ src/script_bdd.sql
@@ -1,6 +1,6 @@
 /* User creation */
-/*CREATE USER 'newuser'@'localhost' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'localhost' IDENTIFIED BY '1234';*/
+/*CREATE USER 'vagrant'@'localhost' IDENTIFIED BY '1234';
+GRANT ALL PRIVILEGES ON yuka_like.* TO 'vagrant'@'localhost' IDENTIFIED BY '1234';*/
 
 /* Database creation */
 CREATE DATABASE yuka_like CHARACTER SET 'utf8';
