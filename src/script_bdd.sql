68f4781721d38ea0f59276f9458fbed3853f175d
mnrdev <mnrdev@mnrs-macbook-pro-1.home>
2019-07-26 23:56:40 +0200

    Ajout: Creation des tables 'Product' et 'History'.

--- src/script_bdd.sql
+++ src/script_bdd.sql
@@ -4,3 +4,28 @@ GRANT ALL PRIVILEGES ON yuka_like.* TO 'vagrant'@'localhost' IDENTIFIED BY '1234
 
 /* Database creation */
 CREATE DATABASE yuka_like CHARACTER SET 'utf8';
+
+/* Tables creation */
+
+CREATE TABLE IF NOT EXISTS Product (
+    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    product_name VARCHAR(200),
+    img_url TEXT,
+    salt VARCHAR(10),
+    fat VARCHAR(10),
+    sugars VARCHAR(10),
+    satured_fat VARCHAR(10),
+    warehouse VARCHAR(30),
+    allergens VARCHAR(30),
+    categories VARCHAR(200),
+
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS History (
+    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    product_id INT NOT NULL,
+
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
