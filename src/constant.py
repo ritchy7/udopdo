d70bf92c85c50baba6c9189a2ac44cacf5e9fe0b
mnrdev <mnrdev@mnrs-macbook-pro-1.home>
2019-08-02 17:28:28 +0200

    Ajout: 'MESSAGE_PROMPT'.

---
+++ src/constant.py
@@ -0,0 +1,6 @@
+MESSAGE_PROMPT = '-------------------------------------------\n'\
+                '1 - Quel aliment souhaitez-vous remplacer ?\n'\
+                '2 - Retrouver mes aliments substitués.\n'\
+                '3 - Synchroniser la base de donnée\n'\
+                'q - Quitter.\n'\
+                '-------------------------------------------\n'
