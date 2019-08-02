252879f88c836d6c1d9c830c0ed2e17f86c7da70
mnrdev <mnrdev@mnrs-macbook-pro-1.home>
2019-08-03 01:03:45 +0200

    Modification: table 'Product' champs warehouse, allergens, category, cle etrangere entre Product et History, Suppression: champ product_id dans la table Product.

--- src/script_bdd.sql
+++ src/script_bdd.sql
@@ -8,26 +8,35 @@ GRANT ALL PRIVILEGES ON openfoodfacts.* TO 'vagrant'@'localhost' IDENTIFIED BY '
 /* Tables creation */
 CREATE TABLE IF NOT EXISTS Product (
     id SMALLINT UNSIGNED AUTO_INCREMENT,
-    product_id INT UNIQUE NOT NULL,
     product_name VARCHAR(200),
     img_url TEXT,
     salt VARCHAR(10),
     fat VARCHAR(10),
     sugars VARCHAR(10),
     saturated_fat VARCHAR(10),
-    warehouse VARCHAR(30),
-    allergens VARCHAR(30),
-    categories VARCHAR(200),
+    warehouse VARCHAR(100),
+    allergens VARCHAR(100),
+    category SMALLINT UNSIGNED UNIQUE NOT NULL,
+
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Category (
+    id SMALLINT UNSIGNED AUTO_INCREMENT,
+    category_name VARCHAR(200),
+    CONSTRAINT fk_category_id
+        FOREIGN KEY (id)
+        REFERENCES Product(category),
 
     PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS History (
     id SMALLINT UNSIGNED AUTO_INCREMENT,
-    product_id INT UNIQUE NOT NULL,
+    product_id SMALLINT UNSIGNED UNIQUE NOT NULL,
     CONSTRAINT fk_product_id
         FOREIGN KEY (product_id)
-        REFERENCES Product(product_id),
+        REFERENCES Product(id),
 
     PRIMARY KEY (id)
 ) ENGINE=InnoDB;
