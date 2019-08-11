bebb0b7b16942025e7ffbadc6775109072b9bafc
mnrdev <mnrdev@mnrs-macbook-pro-1.home>
2019-08-11 15:36:22 +0200

    style(MESSAGE_PROMPT): indentation.

--- src/constant.py
+++ src/constant.py
@@ -1,9 +1,10 @@
 MESSAGE_PROMPT = '-------------------------------------------\n'\
-                '1 - Quel aliment souhaitez-vous remplacer ?\n'\
-                '2 - Retrouver mes aliments substitués.\n'\
-                '3 - Synchroniser la base de donnée\n'\
-                'q - Quitter.\n'\
-                '-------------------------------------------\n'
+    '1 - Quel aliment souhaitez-vous remplacer ?\n'\
+    '2 - Retrouver mes aliments substitués.\n'\
+    '3 - Synchroniser la base de donnée\n'\
+    'q - Quitter.\n'\
+    '-------------------------------------------\n'
+
 
 CINFO = {
     'host': 'localhost',
