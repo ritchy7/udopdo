60911475f95b4e9dcc00b3759a7baad2138af96a
mnrdev <mnrdev@mnrs-macbook-pro-1.home>
2019-08-11 15:40:16 +0200

    refactor: Informations de connexion passer directement à l'initialisation de l'objet, choice became main_menu_choice, menu_choice beamce main_selection_menu.,

--- src/main.py
+++ src/main.py
@@ -3,16 +3,15 @@ from constant import CINFO
 
 if __name__ == '__main__':
     # Init the object.
-    openfoodfacts = OpenFoodFacts()
+    openfoodfacts = OpenFoodFacts(CINFO)
     # Init the connection.
-    openfoodfacts.init_connection(CINFO)
+    openfoodfacts.init_connection()
     # Ask for a choice.
-    openfoodfacts.menu_choice()
-    choice = openfoodfacts.choice
-    if choice == 1:
-        openfoodfacts.select_category()
-    elif choice == 2:
+    openfoodfacts.main_selection_menu()
+    main_menu_choice = openfoodfacts.main_menu_choice
+    if main_menu_choice == 1:
+        openfoodfacts.category_selection_menu()
+    elif main_menu_choice == 2:
         pass
-    elif choice == 3:
+    elif main_menu_choice == 3:
         openfoodfacts.update_database()
-        openfoodfacts.menu_choice()
