c3d495a0676ed44808fa88bddb7d260b23b03b2d
mnrdev <mnrdev@mnrs-macbook-pro-1.home>
2019-08-18 18:35:22 +0200

    refactor(MESSAGE_PROMPT): uppercase 'q'.

--- src/constant.py
+++ src/constant.py
@@ -1,8 +1,8 @@
 MESSAGE_PROMPT = '-------------------------------------------\n'\
     '1 - Quel aliment souhaitez-vous remplacer ?\n'\
     '2 - Retrouver mes aliments substitués.\n'\
-    '3 - Synchroniser la base de donnée\n'\
-    'q - Quitter.\n'\
+    '3 - Synchroniser la base de donnée.\n'\
+    'Q - Quitter.\n'\
     '-------------------------------------------\n'
 
 
