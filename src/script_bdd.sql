b31fb5b055add6291cd32f304b543d977220c565
mnrdev <mnrdev@mnrs-macbook-pro-1.home>
2019-10-13 21:13:47 +0200

    modify(script_bdd.sql): add condition IF NOT EXISTS for all creations, create the user and grant him all privileges on the database.

--- src/script_bdd.sql
+++ src/script_bdd.sql
@@ -1,15 +1,15 @@
 /* Database creation */
-/* CREATE DATABASE openfoodfacts CHARACTER SET 'utf8'; */
+CREATE DATABASE IF NOT EXISTS openfoodfacts CHARACTER SET 'utf8';
 
-/* User creation */
-/*CREATE USER 'vagrant'@'localhost' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON openfoodfacts.* TO 'vagrant'@'localhost' IDENTIFIED BY '1234';*/
+/* User creation & privileges */
+CREATE USER IF NOT EXISTS 'vagrant'@'localhost' IDENTIFIED BY '1234';
+GRANT ALL PRIVILEGES ON openfoodfacts.* TO 'vagrant'@'localhost' IDENTIFIED BY '1234';
 
 /* ****************** */
 /* Tables creation */
 /* ****************** */
 /* Category */
-CREATE TABLE IF NOT EXISTS Category (
+CREATE TABLE IF NOT EXISTS openfoodfacts.Category (
     id SMALLINT UNSIGNED AUTO_INCREMENT,
     category_name VARCHAR(200),
 
@@ -17,10 +17,10 @@ CREATE TABLE IF NOT EXISTS Category (
 ) ENGINE=InnoDB;
 
 /* Product */
-CREATE TABLE IF NOT EXISTS Product (
+CREATE TABLE IF NOT EXISTS openfoodfacts.Product (
     id SMALLINT UNSIGNED AUTO_INCREMENT,
     product_name VARCHAR(200),
-    img_url TEXT,
+    product_url TEXT,
     salt VARCHAR(10),
     fat VARCHAR(10),
     sugars VARCHAR(10),
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS Product (
 ) ENGINE=InnoDB;
 
 /* History */
-CREATE TABLE IF NOT EXISTS History (
+CREATE TABLE IF NOT EXISTS openfoodfacts.History (
     id SMALLINT UNSIGNED AUTO_INCREMENT,
     product_id SMALLINT UNSIGNED UNIQUE NOT NULL,
     CONSTRAINT fk_product_id
