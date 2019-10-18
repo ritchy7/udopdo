c792f239be53b6a73fad6bcaa62856a6069a5354
mnrdev <mnrdev@mnrs-macbook-pro-1.home>
2019-10-18 13:55:39 +0200

    add(constant.py): query to get all product selected.

--- src/constant.py
+++ src/constant.py
@@ -1,3 +1,4 @@
+# Main menu message prompt.
 MESSAGE_PROMPT = ('-------------------------------------------\n'
                   '1 - Quel aliment souhaitez-vous remplacer ?\n'
                   '2 - Retrouver mes aliments substitués.\n'
@@ -5,9 +6,10 @@ MESSAGE_PROMPT = ('-------------------------------------------\n'
                   'Q - Quitter.\n'
                   '-------------------------------------------\n')
 
-
+# Categories url.
 CATEGORIES_URL = 'https://fr.openfoodfacts.org/categories.json'
 
+# Connexion informations.
 CINFO = {
     'host': 'localhost',
     'user': 'vagrant',
@@ -15,8 +17,28 @@ CINFO = {
     'database': 'openfoodfacts'
 }
 
+# Selection product prompt message.
 PRODUCT_PROMPT = ('\n\ns - find a subtitute | p - previous page | r -'
                   ' register\n\nQ - Quitter.\n\n')
 
+#  History product prompt message.
 PRODUCT_PROMPT_HISTORICAL = ('\n\nd - delete the product | p - back to'
                              ' history | Q - Quitter.\n\n')
+
+# Query to retrieve all product choiced.
+GET_HISTORY_QUERY = '''
+    SELECT
+        Product.id,
+        Product.product_name,
+        Product.product_url,
+        Product.salt,
+        Product.fat,
+        Product.sugars,
+        Product.saturated_fat,
+        Product.warehouse,
+        Product.allergens,
+        Product.nutrition_grades
+    FROM Product
+    RIGHT JOIN History
+    ON Product.id = History.product_id;
+'''
