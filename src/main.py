7f51f181def4977a6b223b47a1403116b29c5e76
mnrdev <mnrdev@mnrs-macbook-pro-1.home>
2019-10-18 10:29:33 +0200

    add(main.py): general loop.

--- src/main.py
+++ src/main.py
@@ -7,12 +7,13 @@ if __name__ == '__main__':
     openfoodfacts = OpenFoodFacts(CINFO)
     # Init the connection.
     openfoodfacts.init_connection()
-    # Ask for a choice.
-    openfoodfacts.main_selection_menu()
-    main_menu_choice = openfoodfacts.main_menu_choice
-    if main_menu_choice == 1:
-        openfoodfacts.category_selection_menu()
-    elif main_menu_choice == 2:
-        openfoodfacts.show_saved_products()
-    elif main_menu_choice == 3:
-        openfoodfacts.update_database()
+    while True:
+        # Ask for a choice.
+        openfoodfacts.main_selection_menu()
+        main_menu_choice = openfoodfacts.main_menu_choice
+        if main_menu_choice == 1:
+            openfoodfacts.category_selection_menu()
+        elif main_menu_choice == 2:
+            openfoodfacts.show_saved_products()
+        elif main_menu_choice == 3:
+            openfoodfacts.update_database()
